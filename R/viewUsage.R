0fca438a188311fd5ae993239da0c3453c8f5551
Raj <rraina@stanford.edu>
2019-02-13 18:34:07 -0800

    Preliminary functionality for viewUsage function
    Splits up data set into more manageable pieces for next steps

--- R/viewUsage.R
+++ R/viewUsage.R
@@ -2,7 +2,7 @@
 #'
 #' @description Power usage information for particular household
 #'
-#' @param data data containing 
+#' @param data data containing power information
 #' 
 #' @param type an indication if you want total or average usage
 #' 
@@ -12,21 +12,53 @@
 #'
 #' @export
 #'
-#' @importFrom utils head
-#' @importFrom ggplot2 ggplot aes coord_flip geom_bar scale_fill_manual scale_y_continuous
-#' @importFrom scales percent_format 
-#' @importFrom magrittr %>%
-#' @importFrom dplyr arrange desc group_by inner_join mutate n select slice summarize ungroup
-#' @importFrom tibble tibble
+#' @import utils
+#' @import magrittr
+#' @import dplyr
+#' @import stringr 
+#' @import base
+#' @import datetime
 #'
 #' 
 #' @examples 
-#' viewUsage(householdX, "total", "seasonal")
-#' @keywords dplot
+#' viewUsage(household_data, "total", "seasonal")
+#' @keywords utilities
 
 
-
-viewUsage <- function(data, type, window)
+viewUsage <- function(power_data, type = c("total", "average"), 
+                      window = c("weekly", "monthly", "seasonal"))
 {
+  #DELETE: power_data = read.csv("intervals_7288510005.csv")
   
+  #Make sure data is properly formatted
+  if(any(!(c("interval_start", "interval_end", "interval_kWh", "interval_kW") 
+           %in% colnames(power_data))))
+  {
+    stop("Data must contain columns: 
+         interval_start, interval_end, interval_kWh, interval_kW")
+  }
+  
+  #Subset to only necessary columns
+  power_data <- power_data %>% subset(select = c("interval_start", "interval_end", 
+                                                 "interval_kWh", "interval_kW"))
+  
+  #We will split the interval_start and interval_end columns into two columns: day // hour
+  split_interval_start = str_split(power_data$interval_start, pattern = " ")
+  day_start = sapply(1:nrow(power_data), function(x){split_interval_start[[x]][1]})
+  time_start = sapply(1:nrow(power_data), function(x){split_interval_start[[x]][2]})
+  
+  split_interval_end = str_split(power_data$interval_end, pattern = " ")
+  day_end = sapply(1:nrow(power_data), function(x){split_interval_end[[x]][1]})
+  time_end = sapply(1:nrow(power_data), function(x){split_interval_end[[x]][2]})
+  
+  #Determine the interval length for each period
+  interval_length_hours = difftime(as.POSIXct(power_data$interval_end, format = "%m/%d/%Y %H:%M"), 
+                                   as.POSIXct(power_data$interval_start, format = "%m/%d/%Y %H:%M"), 
+                                   units = "hours")
+  
+  #Create new columns that contain simplified information
+  power_data <- power_data %>% mutate(day_start = day_start, time_start = time_start, 
+                                      day_end = day_end, time_end = time_end,
+                                      interval_length_hours = interval_length_hours) %>% 
+                                      select(-interval_start, -interval_end)
 }
